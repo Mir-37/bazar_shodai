09bd7a93c7a62c2e0fd1daf4167aa0e11b75ab8d
Mir-37 <mirmutasim37@gmail.com>
2026-05-31 04:18:20 +0600

    feat(ui): add navigation to new item screen
    Implement a way to navigate to a new item creation screen by adding
    an action button to the home screen AppBar and creating the
    NewItemScreen widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,36 @@
+import 'package:bazar_shodai/screens/new_item_screen.dart';
 import 'package:bazar_shodai/widgets/grocery_listing.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: const GroceryListing(),
+    );
+  }
+
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
     );
   }
 }
