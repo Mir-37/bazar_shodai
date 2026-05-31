f90ea11c7ab695fbe80aa95c6d4fe3ad0930e507
Mir-37 <mirmutasim37@gmail.com>
2026-05-31 08:01:39 +0600

    refactor(ui): wrap data fetching in try-catch block
    Introduce error handling for network requests in `HomeScreen` by wrapping the HTTP GET call and JSON decoding logic in a try-catch block to prevent application crashes during unexpected failures.
    
    - Add try-catch block around the data fetching logic in `_fetchData`.
    - Update error message to provide a more user-friendly notification upon failure.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,45 +51,51 @@ class _HomeScreenState extends State<HomeScreen> {
       'shopping-list.json',
     );
 
-    final response = await http.get(
-      url,
-    );
-    if (response.statusCode >= 400) {
-      setState(() {
-        _error = 'Failed to fetch data, please try again later';
-      });
-    }
-
-    if (response.body == 'null') {
-      setState(() {
-        _isLoading = false;
-      });
-      return;
-    }
-
-    final Map<String, dynamic> listData = json.decode(
-      response.body,
-    );
-
-    final List<GroceryItem> loadedItems = [];
-
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-            (cItem) => cItem.value.title == item.value['category'],
-          )
-          .value;
-      loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ),
+    try {
+      final response = await http.get(
+        url,
       );
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data, please try again later';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(
+        response.body,
+      );
+
+      final List<GroceryItem> loadedItems = [];
+
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+              (cItem) => cItem.value.title == item.value['category'],
+            )
+            .value;
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+        setState(() {
+          _groceryItems = loadedItems;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
       setState(() {
-        _groceryItems = loadedItems;
-        _isLoading = false;
+        _error = 'Something went wrong, hold on tight..';
       });
     }
   }
